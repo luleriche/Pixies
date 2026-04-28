68e8390054e31a5b3ff5e214d8332047345997ed
smoltofficiel <simongrosse49560@gmail.com>
2026-04-28 15:02:55 +0200

    Ajout des structures fondamental et de leurs fonctions de bases.

--- Graphics/main.cpp
+++ Graphics/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <array>
 
-const unsigned int NMaxCartes = 70;
 
-// Structure pour une carte de jeu
+// Structure pour une carte de jeu.
 struct carte{
     char couleur;
     unsigned int chiffre; // Chiffre sur la carte
@@ -11,29 +10,134 @@ struct carte{
 };
 
 void afficher(carte c){
-    std::cout << "Carte : " << c.couleur << c.chiffre << c.spirale << std::endl;
+    std::cout << c.chiffre << " " << c.couleur << " " << c.spirale;
 }
 
-// Structure pour la défausse/pile de cartes
-struct defausse{
-    std::array<carte, NMaxCartes> tasDeCartes;
+// Définition de la défausse/pile de cartes.
+struct maillon{
+    carte valeur;
+    maillon* suivant;
+};
+using defausse = maillon*;
+
+// Ajoute une carte à la fin de la défausse.
+void ajoutFin(defausse& d, carte c){
+    if(d == nullptr){
+        maillon* nouv = new maillon;
+        nouv->valeur = c;
+        nouv->suivant = d;
+        d = nouv;
+    }else{
+        ajoutFin(d->suivant, c);
+    }
+}
+
+// Structure pour un emplacement de la grille d'un joueur.
+struct emplacementGrille{
+    carte* faceVisible;
+    carte* faceCachee;
+};
+
+// Initialise un emplacement de grille comme étant vide.
+void initEmplacement(emplacementGrille & emplGrille){
+    emplGrille.faceCachee = nullptr;
+    emplGrille.faceVisible = nullptr;
+}
+
+// Affiche un emplacement de grille sous la forme : c. cachée / c. visible.
+void afficher(emplacementGrille emplGrille){
+    if(emplGrille.faceCachee == nullptr)
+        std::cout << "     ";
+    else
+        afficher(*emplGrille.faceCachee);
+    std::cout<< " | ";
+    if(emplGrille.faceVisible == nullptr)
+        std::cout << "     ";
+    else
+        afficher(*emplGrille.faceVisible);
+    std::cout<<std::endl;
+}
+
+
+// Définition de la grille d'un joueur
+using grille = std::array<emplacementGrille, 9>;
+
+// Initialise une grille vide
+void initGrille(grille & g){
+    for( int i = 0; i<9; ++i){
+        initEmplacement(g[i]);
+    }
+}
+
+// Affiche une grille
+void afficherGrille(grille & g){
+    std::cout << std::endl;
+    std::cout << "  |Cachée | Visible" << std::endl;
+    std::cout << "-------------------" << std::endl;
+    for(int i = 0; i < 9; ++i){
+        std::cout<< i+1 << " | ";
+        afficher(g[i]);
+    }
+    std::cout << std::endl;
+}
+
+// Ajoute une carte à la grille 
+void ajouterCarte(grille& g, carte c){
+    std::cout << "----- Ajout de la carte "; afficher(c); std::cout << " -----" << std::endl;
+    // Si il n'y a pas de carte visible à l'emplacement
+    if(g[c.chiffre-1].faceVisible == nullptr){
+        std::cout << "Emplacement de la carte disponible. Carte mise face visible." << std::endl;
+        g[c.chiffre-1].faceVisible = new carte;
+        *g[c.chiffre-1].faceVisible = c;
+
+    // Si il y a une carte visible et pas de cachée
+    }else if(g[c.chiffre-1].faceCachee == nullptr){
+        unsigned int choix;
+        std::cout << "Laissez visible 1 ou 2 ?" << std::endl;
+        std::cout << "1 ("; afficher(*g[c.chiffre-1].faceVisible);
+        std::cout << ") / 2 ("; afficher(c); std::cout << ")" << std::endl;
+        std::cout << "Choix: "; std::cin >> choix;
+        if(choix == 1){
+            g[c.chiffre-1].faceCachee = new carte(c);
+        }else{
+            g[c.chiffre-1].faceCachee = g[c.chiffre-1].faceVisible;
+            g[c.chiffre-1].faceVisible = new carte(c);
+        }
+    }
+    // Si l'emplacement est validé (plein)
+    else{
+        unsigned int choix;
+        std::cout << "Emplacement validé ! Où la mettre ? ";
+        std::cin >> choix;
+        while(g[choix-1].faceCachee != nullptr and g[choix-1].faceVisible){
+            std::cout << "Erreur! Emplacement indisponible. Où la mettre ? ";
+            std::cin >> choix;
+        }
+        g[choix-1].faceCachee = new carte(c);
+    }
+}
+
+// Structure pour la pioche
+struct pioche{
+    std::array<carte,5> cartes;
     unsigned int taille;
 };
-
-// Structure pour un emplacement de la grille d'un joueur
-struct emplacementGrille{
-    carte faceVisible;
-    carte faceCachee;
-};
-
-// Structure pour la grille d'un joueur
-struct grille{
-    std::array<std::array<emplacementGrille, 3>, 3> cases;
-};
-
 
 
 int main()
 {
+    grille g;
+    initGrille(g);
+    carte c1 = {'r', 4, 2};
+    carte c2 = {'j', 4, 1};
+    carte c3 = {'b', 4, 7};
+    carte c4 = {'r', 7, 2};
+    afficherGrille(g);
+    ajouterCarte(g, c1);
+    afficherGrille(g);
+    ajouterCarte(g, c2);
+    afficherGrille(g);
+    ajouterCarte(g, c3);
+    afficherGrille(g);
     return 0;
 }
